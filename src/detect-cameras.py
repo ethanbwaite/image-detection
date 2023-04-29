59a5eb5e06fa9adc0febdf3b6233d99d508ace9e
ethanbwaite <ebwaite@calpoly.edu>
2023-04-29 14:55:07 -0700

    Detect cameras

---
+++ src/detect-cameras.py
@@ -0,0 +1,16 @@
+import cv2, sys
+
+def select_camera():
+    num_cameras = sys.argv[1]  # Change this to the number of cameras you want to try
+    for i in range(num_cameras):
+        cap = cv2.VideoCapture(i)
+        ret, frame = cap.read()
+        if ret:
+            print("Selected camera: ", i)
+            return cap
+        else:
+            cap.release()
+    print("Could not find any camera. Exiting.")
+    exit()
+
+select_camera
