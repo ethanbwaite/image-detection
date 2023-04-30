5e61f714b972edde1e3af3963a1b45ac6f470570
ethanbwaite <ebwaite@calpoly.edu>
2023-04-29 21:10:52 -0700

    h264

--- src/server.py
+++ src/server.py
@@ -32,7 +32,7 @@ class VideoServer:
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
         cap.set(cv2.CAP_PROP_FPS, 30)
-        cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'))
         
         period = 10 
         tick = 0
